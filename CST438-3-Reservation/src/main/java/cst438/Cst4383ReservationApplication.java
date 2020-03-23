c739b3d9827642e4d9e3997599c036989bbf4602
ynikulyak <45584946+ynikulyak@users.noreply.github.com>
2020-03-22 23:22:12 -0700

    Add files via upload

---
+++ CST438-3-Reservation/src/main/java/cst438/Cst4383ReservationApplication.java
@@ -0,0 +1,13 @@
+package cst438;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst4383ReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst4383ReservationApplication.class, args);
+	}
+
+}
---
+++ CST438-3-Reservation/src/test/java/cst438/Cst4383ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst4383ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
